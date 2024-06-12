7f6802bb50cf838293cb90442eea5be2c397ef23
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-12 13:18:13 +0530

    setting up the react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Accordian from './components/accordian';
+import Loginform from './components/inputForm/input';
+import RandomColor from './components/random-color';
+import StarRating from './components/stra-rating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Accordian/> */}
+      {/* <RandomColor/> */}
+      {/* <StarRating /> */}
+      <Loginform/>
+    </div>
+  );
+}
+
+export default App;

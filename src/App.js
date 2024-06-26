5642e21e7f7507fa7ec9fa655869ff7d61e5b3d7
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-27 01:15:50 +0530

    add qr-generator, autosearch and random color components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Accordian from "./components/accordian";
-import Loginform from "./components/inputForm/input";
+// import Loginform from "./components/inputForm/input";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/stra-rating";
+import QRCodeGenerator from "./components/qr-code-generator";
+import SearchAutocomplete from "./components/search-autocomplete-with-apis";
 
 function App() {
   return (
     <div className="App">
       <Accordian /> <hr />
       <StarRating /> <hr />
+      <QRCodeGenerator /> <hr />
+      <SearchAutocomplete /> <hr />
+      <RandomColor /> <hr />
     </div>
   );
 }
